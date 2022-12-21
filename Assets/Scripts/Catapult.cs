5a9b7f3b43451507d3a7006778b25e4573ef5de7
siddgoel <siddharth.goel756@gmail.com>
2022-12-21 00:04:18 -0500

    Update catapult stuff

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -4,96 +4,93 @@ using UnityEngine;
 
 public class Catapult : MonoBehaviour
 {
-    // Angular speed in radians per sec.
-    public float speed = 1.0f;
     public GameObject weapon_prefab;
 
-    private float shooting_delay; 
-    private Vector3 direction_from_turret_to_target;
+    private float shooting_delay;
     private Vector3 shooting_direction;
-    private Vector3 projectile_starting_pos;
-    private float projectile_velocity;
+    private Vector3 weapon_starting_pos;
+    private float weapon_velocity;
+    private bool target_in_range;
     private GameObject target;
     private Vector3 target_position;
+    private float distance_to_target = 0.0f;
+    private float min_dist = 10.0f;
+    private float max_dist = 30.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("player");
-        target_position = target.transform.position;
         if (weapon_prefab == null)
-            Debug.LogError("Error: could not find the barrel prefab in the project! Did you delete/move the prefab from your project?");
-        shooting_delay = 0.5f;  
-        projectile_velocity = 5.0f;
-        direction_from_turret_to_target = new Vector3(0.0f, 0.0f, 0.0f);
-        projectile_starting_pos = transform.position;
+            Debug.LogError("Error: could not find the weapon prefab in the project! Did you delete/move the prefab from your project?");
+        shooting_delay = 1.0f;  
+        weapon_velocity = 5.0f;
+        weapon_starting_pos = new Vector3(0.0f, 0.0f, 0.0f);
+        target_in_range = false;
         StartCoroutine("Spawn");
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        // // Determine which direction to rotate towards
-        // Vector3 targetDirection = target.transform.position - transform.position;
+        if (target == null)
+            Debug.LogError("Error: could not find the game character 'player' in the scene. Did you delete the model player from your scene?");
+        
+        Vector3 target_centroid = target.GetComponent<BoxCollider>().bounds.center;
+        Vector3 catapult_centroid = transform.position;
+        distance_to_target = Vector3.Distance(target_centroid, catapult_centroid);
 
-        // // The step size is equal to speed times frame time.
-        // float singleStep = speed * Time.deltaTime;
+        // Here, we rotate the catapult in the direction of the target
+        Vector3 direction_to_target = target_centroid - catapult_centroid;
+        direction_to_target.Normalize();
+        float angle_to_rotate_catapult = Mathf.Rad2Deg * Mathf.Atan2(shooting_direction.x, shooting_direction.z);
+        transform.eulerAngles = new Vector3(0.0f, angle_to_rotate_catapult, 0.0f);
 
-        // // Rotate the forward vector towards the target direction by one step
-        // Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-        // // Draw a ray pointing at our target in
-        // Debug.DrawRay(transform.position, newDirection, Color.red);
-
-        // transform.rotation = Quaternion.LookRotation(newDirection);
-
-        // Vector3 target_velocity = (target.transform.position - target_position) / Time.deltaTime;
-        // shooting_direction = newDirection;
-        // shooting_direction.Normalize();
-
-        // Vector3 current_turret_direction = new Vector3(Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.y), 1.1f, Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.y));
-        // projectile_starting_pos = transform.position + 1.1f * current_turret_direction;  // estimated position of the turret's front of the cannon
-        Vector3 target_centroid = target.GetComponent<CapsuleCollider>().bounds.center;
-        // Vector3 turret_centroid = GetComponent<"ForestCataPultArm">.GetComponent<SphereCollider>().GetComponent<>.bounds.center;
-        Vector3 turret_centroid = transform.position;
-        // direction_from_turret_to_target = target_centroid - turret_centroid;
-        // direction_from_turret_to_target.Normalize();
-
-        Vector3 target_velocity = (target.transform.position - target_position) / Time.deltaTime;
-        target_position = target.transform.position;
-        shooting_direction = iterative_solution(target, target_centroid, turret_centroid, projectile_velocity);
-        shooting_direction.Normalize();
-        ////////////////////////////////////////////////
-
-        float angle_to_rotate_turret = Mathf.Rad2Deg * Mathf.Atan2(shooting_direction.x, shooting_direction.z);
-        transform.eulerAngles = new Vector3(0.0f, angle_to_rotate_turret, 0.0f);
-        Vector3 current_turret_direction = new Vector3(Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.y), 1.1f, Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.y));
-        projectile_starting_pos = transform.position + 1.1f * current_turret_direction;  // estimated position of the turret's front of the cannon
+        if (distance_to_target >= min_dist && distance_to_target <= max_dist)
+            target_in_range = true;
+        else
+            target_in_range = false;
     }
 
     private IEnumerator Spawn()
     {
         while (true)
-        {       
-            Debug.Log("Spawning new weapon");
-            GameObject new_object = Instantiate(weapon_prefab, projectile_starting_pos, Quaternion.identity);
-            if(new_object == null) {
-                Debug.LogError("NULL HAI OBJECT");
+        {            
+            if(target_in_range) {
+                // Defining the catapult object
+                GameObject catapult_arm_base = transform.Find("CatapultArmBase").gameObject;
+                GameObject catapult_arm = catapult_arm_base.transform.Find("CatapultArm").gameObject;
+                GameObject catapult_sphere = catapult_arm.transform.Find("CatapultSphere").gameObject;
+                
+                // Then we start to take the aim => Catapult starts to launch and rotates its arm
+                catapult_arm_base.transform.rotation = Quaternion.Euler(80.0f, 0, 0);
+                weapon_starting_pos = catapult_sphere.transform.position;
+
+                // Let's define the initial and final positions first
+                Vector3 initialPos = weapon_starting_pos;
+                Vector3 finalPos = target.transform.position;
+                Vector3 displacement = finalPos - initialPos;
+
+                // Calculate time taken
+                float time = Mathf.Sqrt(Mathf.Abs(2 * displacement.y / 9.81f));
+                Debug.Log("displacement: " + displacement);
+                Debug.Log("time: " + time);
+                Vector3 initial_velocity = displacement / time;
+                initial_velocity.y = 0;
+
+                // Now that we have our initial velocities, let us initiate the attack
+                GameObject new_object = Instantiate(weapon_prefab, weapon_starting_pos, Quaternion.identity);
+                new_object.GetComponent<Rock>().velocity = initial_velocity;
+                new_object.GetComponent<Rock>().birth_time = Time.time;
+                new_object.GetComponent<Rock>().birth_catapult = transform.gameObject;
+                new_object.GetComponent<Rock>().initialPos = weapon_starting_pos;
+
+                // Bringing back the catapult arm to its original position
+                catapult_arm_base.transform.rotation = Quaternion.Euler(-80.0f, 0, 0);
             }
-            new_object.AddComponent<Weapon>();
-            new_object.GetComponent<Weapon>().direction = shooting_direction;
-            new_object.GetComponent<Weapon>().velocity = projectile_velocity;
-            new_object.GetComponent<Weapon>().birth_time = Time.time;
-            new_object.GetComponent<Weapon>().birth_turret = transform.gameObject;
+            // Add some delay, we don't want to bombard the kid
+            yield return new WaitForSeconds(shooting_delay);
         }
-        yield return new WaitForSeconds(shooting_delay); // next shot will be shot after this delay
     }
-
-    Vector3 iterative_solution(GameObject target, Vector3 target_centroid, Vector3 turret_centroid, float projectile_speed) {
-        Vector3 future_pos = target_centroid;
-
-        return (future_pos - turret_centroid);
-    }
-
-
 }
--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -2,27 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Rock : MonoBehaviour
 {
-    public Vector3 direction;
-    public float velocity;
+    public Vector3 velocity;
     public float birth_time;
-    public GameObject birth_turret;
+    public GameObject birth_catapult;
+    public Vector3 initialPos;
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hereaaaa");
-        if (Time.time - birth_time > 10.0f)  // weapon live for 10 sec max
+        float time_since_launch = Time.time - birth_time;
+        if (time_since_launch > 10.0f)  // weapon lives for 10 sec max
         {
             Destroy(transform.gameObject);
         }
-        transform.position = transform.position + velocity * direction * Time.deltaTime;
+        float t = Time.deltaTime;
+        Vector3 finalPos = new Vector3(0, 0, 0);
+        finalPos.x = transform.position.x + velocity.x * t;
+        finalPos.y = initialPos.y - 0.5f * 9.81f * time_since_launch * time_since_launch;
+        finalPos.z = transform.position.z + velocity.z * t;
+        transform.position = finalPos;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,7 +38,7 @@ public class Weapon : MonoBehaviour
         // (a) if the object collides with Claire, subtract one life from her, and destroy the apple => wrote this logic in the Claire.cs file
         // (b) if the object collides with another apple, or its own turret that launched it (birth_turret), don't do anything
         // (c) if the object collides with anything else (e.g., terrain, a different turret), destroy the apple
-        if(!(other.gameObject.name == birth_turret.name || other.gameObject.name == "Apple" || other.gameObject.name == "Apple(Clone)")) {
+        if(!(other.gameObject.name == birth_catapult.name || other.gameObject.name == "Apple" || other.gameObject.name == "Apple(Clone)")) {
             Debug.Log("Hello " + other.gameObject.name);
             Destroy(this.gameObject);
         }
